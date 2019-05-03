5f3154249c68f7861c40bf257ffefd869c1ab181
Jorge Hristho Precich Serna <precichysim@gmail.com>
2019-05-02 22:18:44 -0500

    add login

--- src/Component/SignIn.js
+++ src/Component/SignIn.js
@@ -1,25 +1,24 @@
 import React, { Component } from "react"
 import { Form, Icon, Input, Button, Typography } from 'antd'
-import '../Style/SignUp.css'
+import '../Style/SignIn.css'
 import axios from 'axios'
 
 const { Title } = Typography
 
-class SignUp extends Component {
+class SignIn extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     this.validateFields()
       .then((user) => {
-        axios.post('http://localhost:8000/api/v1/users', {
-          username: user.username,
+        axios.post('http://localhost:8000/api/v1/login', {
           email: user.email,
           password: user.password
         })
         .then((res) => {
           if (Object.keys(res.data).length !== 0 && res.data.constructor === Object) {
-              window.location = "/login"
+            window.location = "/"
           } else {
-              window.location = "/signin"
+            window.location = "/signin"
           }
         })
         .catch(err => {
@@ -44,8 +43,8 @@ class SignUp extends Component {
 
     return (
       <div>
-        <Title>Sign Up</Title>
-        <Form onSubmit={this.handleSubmit} className="signup-form">
+        <Title>Sign In</Title>
+        <Form onSubmit={this.handleSubmit} className="signin-form">
           <Form.Item>
             {getFieldDecorator('email', {
               rules: [{required: true, message: 'Please input an email'}],
@@ -54,26 +53,19 @@ class SignUp extends Component {
               )}
           </Form.Item>
           <Form.Item>
-            {getFieldDecorator('username', {
-              rules: [{ required: true, message: 'Please input an username' }],
-            })(
-              <Input prefix={<Icon className="icon" type="user" />} placeholder="Username" />
-            )}
-          </Form.Item>
-          <Form.Item>
             {getFieldDecorator('password', {
               rules: [{ required: true, message: 'Please input your Password!' }],
             })(
               <Input type="password" prefix={<Icon className="icon" type="lock" />} placeholder="Password" />
             )}
           </Form.Item>
-          <Button htmlType="submit">Save</Button>
+          <Button htmlType="submit">Submit</Button>
         </Form>
       </div>
     )
   }
 }
 
-const WrappedSignup = Form.create()(SignUp)
+const WrappedSignin= Form.create()(SignIn)
 
-export default WrappedSignup
+export default WrappedSignin
