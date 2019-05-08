cac209c20351dd2178059bd5de810892dbdcdeaf
Jorge Hristho Precich Serna <precichysim@gmail.com>
2019-05-07 21:38:22 -0500

    feat create posts

--- src/Component/GroupFeed.js
+++ src/Component/GroupFeed.js
@@ -1,38 +1,16 @@
-import React, { Component } from "react"
-import { Form, Icon, Input, Button, Modal } from 'antd'
-import '../Style/GroupModal.css'
+import React, { Component } from 'react';
+import PostForm from './PostForm' 
 
-const GroupModal = Form.create({ name: 'group_modal' })(
-  class extends Component {
-    render() {
-      const {
-        visible, onCancel, onCreate, form,
-      } = this.props;
-      const { getFieldDecorator } = form;
-      return (
-        <Modal
-          visible={visible}
-          title="Create Group"
-          onCancel={onCancel}
-          footer={null}
-          centered
-        >
-          <Form onSubmit={onCreate} className="group-modal-form">
-            <Form.Item>
-              {getFieldDecorator('name', {
-                rules: [{required: true, message: 'Please input a name'}],
-                })(
-              <Input prefix={<Icon className="icon" type="user" />} placeholder="Name" />
-                )}
-            </Form.Item>
-            <Button htmlType="submit">Save</Button>
-          </Form>
-        </Modal>
-      );
-    }
+import '../Style/GroupFeed.css';
+
+
+class GroupFeed extends Component {
+  render() {
+    return (
+      <div>
+        <PostForm></PostForm>
+      </div>
+    );
   }
-);
-
-const WrappedGroupModal = Form.create()(GroupModal)
-
-export default WrappedGroupModal
+}
+export default GroupFeed;
